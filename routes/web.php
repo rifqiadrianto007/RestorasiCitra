161d298b5bfaa5145ad15d66b22b4e16869d9aad
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-10-16 21:44:19 +0700

    delete remove routes

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
 
-Route::get('/', function () {
-    return view('landing');
-});
-
 Route::get('/smooth-form', function () {
     return view('smooth');
 });
 
-Route::get('/remove-form', function () {
-    return view('remove');
-});
-
 Route::post('/smooth', [ImageController::class, 'smooth'])->name('smooth.process');
-Route::post('/remove-background', [ImageController::class, 'removeBackground'])->name('remove.process');
